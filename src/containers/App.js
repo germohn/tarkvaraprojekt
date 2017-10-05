a493b9f77126387567c0e5e8607f5e24043b2c94
Germo Hynerson <germohn@gmail.com>
2017-10-05 09:02:37 +0300

    beginning of tableview component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
-
+import R from 'ramda'
 import Table from './table/Table';
 import Card from './card/Card';
 import Statistics from './statistics/Statistics';
+import mockData from '../mockData';
+
 
 const App = () => {
+
+  let tags =
+  console.log('mockData', mockData);
+  const sample = mockData.slice(0, 20);
+
   return (
-    <div className="app">
-      <div className="app-header">
-        <h1>Funderbeam data</h1>
-      </div>
-      <h2>Here comes 3 views for representing data</h2>
-      <p>somehow implemented in tabs or smth...</p>
-      <Table/>
-      <Card/>
-      <Statistics/>
-    </div>
+    <div className="app" >
+      <div className="app-header" >
+        <h1 >Funderbeam data</h1 >
+      </div >
+      <h2 >Here comes 3 views for representing data</h2 >
+      <p >somehow implemented in tabs or smth...</p >
+      <Table data={sample} />
+      <Card />
+      <Statistics />
+    </div >
   );
 };
 
--- src/containers/table/Table.js
+++ src/containers/table/Table.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import CompanyRow from '../../components/table/CompanyRow';
+import Table from 'react-bootstrap/lib/Table';
 
-const Table = () => {
+const TableView = (props) => {
+  console.log('table props', props)
   return (
-    <div>
-      <h3>Table view</h3>
-    </div>
-  )
+    <div >
+      <h3 >Table view</h3 >
+      <Table responsive bordered >
+        <thead>
+        <tr >
+          <th >Company</th >
+          <th >Funding</th >
+          <th >Employees</th >
+          <th >Tags</th >
+        </tr >
+        </thead>
+        <tbody >
+        {/*<tr >*/}
+          {/*<td >{props.data[0].name}</td >*/}
+          {/*<td >{props.data[0].funding}</td >*/}
+          {/*<td >{props.data[0].employees}</td >*/}
+          {/*<td >{props.data[0].tags}</td >*/}
+        {/*</tr >*/}
+        {props.data.map(comp => {
+          return (<CompanyRow company={comp} />)
+        })}
+        </tbody >
+
+      </Table >
+
+    </div >
+  );
 };
 
-export default Table;
+export default TableView;
