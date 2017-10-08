cf5c997b0a1670d9898850c67f3f2520e05cbe20
germo <germohn@gmail.com>
2017-10-09 00:18:40 +0300

    basic table view functionality and sorting

--- src/components/table/CompanyRow.js
+++ src/components/table/CompanyRow.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 class CompanyRow extends React.Component {
-  constructor(props) {
-    super(props)
-    console.log(props.company)
-  }
+    constructor(props) {
+        super(props)
+        // console.log(props.company)
+    }
 
-  render() {
-    return (
-      <tr >
-        <td >{this.props.company.name}</td >
-        <td >{this.props.company.funding ? this.props.company.funding + ' $' : '-'}</td >
-        <td >{this.props.company.employees}</td >
-        <td >{this.props.company.tags}</td >
-      </tr >
-    );
-  }
+    render() {
+        return (
+            <tr>
+                <td>{this.props.company.name}</td>
+                <td>{this.props.company.funding ? this.props.company.funding + ' $' : '-'}</td>
+                <td>{this.props.company.employees}</td>
+                <td>{this.props.company.tags}</td>
+            </tr>
+        );
+    }
 
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -8,22 +8,29 @@ import mockData from '../mockData';
 
 const App = () => {
 
-  let tags =
-  console.log('mockData', mockData);
-  const sample = mockData.slice(0, 20);
 
-  return (
-    <div className="app" >
-      <div className="app-header" >
-        <h1 >Funderbeam data</h1 >
-      </div >
-      <h2 >Here comes 3 views for representing data</h2 >
-      <p >somehow implemented in tabs or smth...</p >
-      <Table data={sample} />
-      <Card />
-      <Statistics />
-    </div >
-  );
+    console.log('The whole dataSet: ', mockData);
+    const sample = mockData.slice(0, 20);
+
+    const allTags = R.flatten(mockData.map(comp => {
+        return comp.tags
+    }));
+    // console.log(allTags);
+    // const temp = R.countBy(R.uniq)(allTags)
+    console.log('All unique Tags in the dataSet: ',R.uniq(allTags));
+
+    return (
+        <div className="app">
+            <div className="app-header">
+                <h1>Funderbeam data</h1>
+            </div>
+            <h2>Here comes 3 views for representing data</h2>
+            <p>somehow implemented in tabs or smth...</p>
+            <Table data={sample}/>
+            <Card/>
+            <Statistics/>
+        </div>
+    );
 };
 
 export default App;
