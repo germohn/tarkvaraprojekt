ba19d5e70a8bf403dea60f815fa59c3019429175
germo <germohn@gmail.com>
2017-10-16 01:41:15 +0300

    Table view tags filter

--- src/containers/table/Table.js
+++ src/containers/table/Table.js
@@ -5,7 +5,6 @@ import CompanyRow from '../../components/table/CompanyRow';
 
 const changeOrder = (array, sortBy, order) => {
     const newOrder = R.sortBy(R.compose(R.toLower, R.prop(sortBy)))(array);
-    // console.log(newOrder);
     if (order === 'desc') {
         return newOrder;
     }
@@ -15,10 +14,10 @@ const changeOrder = (array, sortBy, order) => {
 const changeNumOrder = (array, sortBy, order) => {
     const undefinedArray = [];
     const definedArray = [];
-    for (let i = 0; i<array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         if (array[i][sortBy] == null) {
             undefinedArray.push(array[i]);
-        } else{
+        } else {
             definedArray.push(array[i]);
         }
     }
@@ -29,22 +28,40 @@ const changeNumOrder = (array, sortBy, order) => {
     return R.reverse(undefinedArray.concat(newOrder));
 };
 
+const filterCompaniesWithTags = (companies, tags) => {
+    if (tags.length <= 0) {
+        return companies;
+    } else {
+        let comps = [];
+        tags.forEach((tag) => {
+            companies.forEach((comp) => {
+                if (comp.tags && comp.tags.includes(tag)) {
+                    comps.push(comp);
+                }
+            });
+        });
+        return R.uniq(comps);
+    }
+};
+
 class TableView extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            companies: props.data,
             sortBy: 'name',
-            order: null
+            order: null,
+            unSelectedTags: props.tags,
+            selectedTags: [],
+            companies: props.data
         };
-        // console.log(this.state.companies)
 
         this.handleNameClick = this.handleNameClick.bind(this);
+        this.handleTagSelect = this.handleTagSelect.bind(this);
     }
 
     handleNameClick(e) {
-        let newState = R.merge(this.state);
+        let newState = R.clone(this.state);
         if (this.state.order === 'asc' || this.state.order === null) {
             newState = R.merge(newState, {companies: changeOrder(this.state.companies, 'name', 'desc'), order: 'desc'});
         } else {
@@ -54,51 +71,105 @@ class TableView extends React.Component {
     }
 
     handleFundingClick(e) {
-        let newState = R.merge(this.state);
-        if (this.state.order === 'desc' || this.state.order === null ) {
-            newState = R.merge(newState, {companies: changeNumOrder(this.state.companies,
-                'funding', 'asc'), order: 'asc'});
+        let newState = R.clone(this.state);
+        if (this.state.order === 'desc' || this.state.order === null) {
+            newState = R.merge(newState, {
+                companies: changeNumOrder(this.state.companies,
+                    'funding', 'asc'), order: 'asc'
+            });
         } else {
-            newState = R.merge(newState, {companies: changeNumOrder(this.state.companies,
-                'funding', 'desc'), order: 'desc'});
+            newState = R.merge(newState, {
+                companies: changeNumOrder(this.state.companies,
+                    'funding', 'desc'), order: 'desc'
+            });
         }
         this.setState({companies: newState.companies, sortBy: 'funding', order: newState.order});
     }
 
     handleEmployeesClick(e) {
-        let newState = R.merge(this.state);
-        if (this.state.order === 'desc' || this.state.order === null ) {
-            newState = R.merge(newState, {companies: changeNumOrder(this.state.companies,
-                'employees', 'asc'), order: 'asc'});
+        let newState = R.clone(this.state);
+        if (this.state.order === 'desc' || this.state.order === null) {
+            newState = R.merge(newState, {
+                companies: changeNumOrder(this.state.companies,
+                    'employees', 'asc'), order: 'asc'
+            });
         } else {
-            newState = R.merge(newState, {companies: changeNumOrder(this.state.companies,
-                'employees', 'desc'), order: 'desc'});
+            newState = R.merge(newState, {
+                companies: changeNumOrder(this.state.companies,
+                    'employees', 'desc'), order: 'desc'
+            });
         }
         this.setState({companies: newState.companies, sortBy: 'funding', order: newState.order});
     }
 
     handleFoundedClick(e) {
-        let newState = R.merge(this.state);
-        if (this.state.order === 'desc' || this.state.order === null ) {
-            newState = R.merge(newState, {companies: changeNumOrder(this.state.companies,
-                'foundedOn', 'asc'), order: 'asc'});
+        let newState = R.clone(this.state);
+        if (this.state.order === 'desc' || this.state.order === null) {
+            newState = R.merge(newState, {
+                companies: changeNumOrder(this.state.companies,
+                    'foundedOn', 'asc'), order: 'asc'
+            });
         } else {
-            newState = R.merge(newState, {companies: changeNumOrder(this.state.companies,
-                'foundedOn', 'desc'), order: 'desc'});
+            newState = R.merge(newState, {
+                companies: changeNumOrder(this.state.companies,
+                    'foundedOn', 'desc'), order: 'desc'
+            });
         }
         this.setState({companies: newState.companies, sortBy: 'funding', order: newState.order});
     }
 
 
+    handleTagSelect(tag) {
+        const unSeleceted = this.state.unSelectedTags;
+        const index = unSeleceted.indexOf(tag);
+        unSeleceted.splice(index, 1);
+        const selected = this.state.selectedTags;
+        selected.push(tag);
+        this.setState({
+            unSelectedTags: unSeleceted,
+            selectedTags: selected
+        });
+    }
+
+    handleTagDeselect(tag) {
+        const selected = this.state.selectedTags;
+        const index = selected.indexOf(tag);
+        selected.splice(index, 1);
+        let unSelected = this.state.unSelectedTags;
+        unSelected.push(tag);
+
+        this.setState({
+            unSelectedTags: unSelected,
+            selectedTags: selected
+        });
+    }
+
+
     render() {
-        // const sortByNameCaseInsensitive = R.sortBy(R.compose(R.toLower, R.prop('name')));
-        // console.log(this.state.companies['name'])
-        // console.log(sortByNameCaseInsensitive(this.state.companies))
+        const filteredCompanies = filterCompaniesWithTags(this.state.companies, this.state.selectedTags);
         return (
-            <div>
-                <h3>Table view</h3>
-                <table>
-                    <thead>
+            <div className="container">
+                <div className="row">
+                    {
+                        this.state.unSelectedTags.map((tag, i) => {
+                            return (
+                                <div className="chip" key={i} onClick={(e) => this.handleTagSelect(tag)}>{tag}</div>);
+                        })
+                    }
+                </div>
+                <div className="row">
+                    <h4>selected</h4>
+                    {
+                        this.state.selectedTags.map((tag, i) => {
+                            return (
+                                <div className="chip" key={i} onClick={(e) => this.handleTagDeselect(tag)}>{tag}</div>);
+                        })
+                    }
+                </div>
+                <div className="row">
+                    <h3>Table view</h3>
+                    <table>
+                        <thead>
                         <tr>
                             <th onClick={(e) => this.handleNameClick(e)}>Company</th>
                             <th>Description</th>
@@ -108,21 +179,24 @@ class TableView extends React.Component {
                             <th>Stage</th>
                             <th onClick={(e) => this.handleFoundedClick(e)}>Founded</th>
                         </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.companies.map((comp) => {
-                        return (<CompanyRow key={comp.slug} company={comp}/>);
-                    })}
-                    </tbody>
-                </table>
-                <button className = 'showAll' type="button">Show all</button>
+                        </thead>
+                        <tbody>
+                        {filteredCompanies.map((comp) => {
+                            return (<CompanyRow key={comp.slug} company={comp}/>);
+                        })
+                        }
+                        </tbody>
+                    </table>
+                    <button className='showAll' type="button">Show all</button>
+                </div>
             </div>
         );
     }
 }
 
 TableView.propTypes = {
-    data: PropTypes.arrayOf(React.PropTypes.object).isRequired
+    data: PropTypes.arrayOf(React.PropTypes.object).isRequired,
+    tags: PropTypes.array.isRequired
 };
 
 export default TableView;
