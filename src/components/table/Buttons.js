4ae1f02b797402756866e32321c3025dcb8489ea
Madli Urbanik <madliurbanik@gmail.com>
2017-10-17 19:50:01 +0300

    fixes in button component, see #13

--- src/components/table/Buttons.js
+++ src/components/table/Buttons.js
@@ -3,25 +3,18 @@ import PropTypes from 'prop-types';
 
 
 const Buttons = (props) => {
-  const showButton = (limit, length) => {
-    if (limit < length) {
-      return (
-        <div>
-          <button className='showAll' type="button" onClick={(e) => props.showMore(e)}>Show more</button>
-          <button className='showAll' type="button" onClick={(e) => props.showAll(e)}>Show all</button>
-        </div>);
-    } else {
-      return (
-        <div>
-          <p>No more results to show...</p>
-        </div>);
-    }
-  };
-  return (
-    <div>
-      {showButton(props.limit, props.length)
-      } </div>
-  );
+  {if (props.limit < props.length) {
+    return (
+      <div>
+        <button className='showAll' type="button" onClick={(e) => props.showMore(e)}>Show more</button>
+        <button className='showAll' type="button" onClick={(e) => props.showAll(e)}>Show all</button>
+      </div>);
+  } else {
+    return (
+      <div>
+        <p>No more results to show...</p>
+      </div>);
+  }}
 };
 
 Buttons.propTypes = {
