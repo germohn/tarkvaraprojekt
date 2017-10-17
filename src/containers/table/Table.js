974c215ae51c0c3ceac6ac0730f285d63cb47eb4
germo <germohn@gmail.com>
2017-10-17 10:21:08 +0300

    Fixed merge conflicts and errors, should be fine now

--- src/containers/table/Table.js
+++ src/containers/table/Table.js
@@ -29,69 +29,68 @@ const changeNumOrder = (array, sortBy, order) => {
 };
 
 
-const filterCompanies= (companies, stages, tags) => {
-    if (stages.length <= 0 && tags.length <= 0) {
-        return companies;
-    }else if(stages.length <= 0 && !(tags.length <= 0)) {
-        let comps = [];
-        tags.forEach((tag) => {
-            companies.forEach((comp) => {
-                if (comp.tags && comp.tags.includes(tag)) {
-                    comps.push(comp);
-                }
-            });
+const filterCompanies = (companies, stages, tags) => {
+  if (stages.length <= 0 && tags.length <= 0) {
+    return companies;
+  } else if (stages.length <= 0 && !(tags.length <= 0)) {
+    let comps = [];
+    tags.forEach((tag) => {
+      companies.forEach((comp) => {
+        if (comp.tags && comp.tags.includes(tag)) {
+          comps.push(comp);
+        }
+      });
+    });
+    return R.uniq(comps);
+  } else if (!(stages.length <= 0) && tags.length <= 0) {
+    let comps = [];
+    stages.forEach((stage) => {
+      companies.forEach((comp) => {
+        if (comp.stage && stages.includes(comp.stageName)) {
+          comps.push(comp);
+        }
+      });
+    });
+    return R.uniq(comps);
+  } else if (!(stages.length <= 0) && !(tags.length <= 0)) {
+    let comps = [];
+    stages.forEach((stage) => {
+      tags.forEach((tag) => {
+        companies.forEach((comp) => {
+          if (comp.stage && stages.includes(comp.stageName) && comp.tags && comp.tags.includes(tag)) {
+            comps.push(comp);
+          }
         });
-        return R.uniq(comps);
-    }else if(!(stages.length <= 0) && tags.length <= 0) {
-        let comps = [];
-        stages.forEach((stage) => {
-            companies.forEach((comp) => {
-                if (comp.stage && stages.includes(comp.stageName)) {
-                    comps.push(comp);
-                }
-            });
-        });
-        return R.uniq(comps);
-    } else if(!(stages.length <= 0) && !(tags.length <= 0)) {
-        let comps = [];
-        stages.forEach((stage) => {
-            tags.forEach((tag) => {
-                companies.forEach((comp) => {
-                    if (comp.stage && stages.includes(comp.stageName) && comp.tags && comp.tags.includes(tag)) {
-                        comps.push(comp);
-                    }
-                });
-            });
-        });
-        return R.uniq(comps);
-    }
+      });
+    });
+    return R.uniq(comps);
+  }
 };
 
 class TableView extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            sortBy: 'name',
-            order: 'desc',
-            showCount: 20,
-            unSelectedTags: props.tags.sort(
-                (a, b) => {
-                    return a.toLowerCase().localeCompare(b.toLowerCase());
-                }
-            ),
-            selectedTags: [],
-            unSelectedStages: props.stages,
-            selectedStages: [],
-            companies: R.sortBy(R.compose(R.toLower, R.prop('name')))(props.data),
-            search: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortBy: 'name',
+      order: 'desc',
+      showCount: 20,
+      unSelectedTags: props.tags.sort(
+        (a, b) => {
+          return a.toLowerCase().localeCompare(b.toLowerCase());
+        }
+      ),
+      selectedTags: [],
+      unSelectedStages: props.stages,
+      selectedStages: [],
+      companies: R.sortBy(R.compose(R.toLower, R.prop('name')))(props.data),
+      search: ''
 
-        };
-        // console.log(this.state.companies)
+    };
 
-        this.handleNameClick = this.handleNameClick.bind(this);
-        this.handleTagSelect = this.handleTagSelect.bind(this);
-        this.handleStageSelect = this.handleStageSelect.bind(this);
+    this.handleNameClick = this.handleNameClick.bind(this);
+    this.handleTagSelect = this.handleTagSelect.bind(this);
+    this.handleStageSelect = this.handleStageSelect.bind(this);
     this.updateSearch = this.updateSearch.bind(this);
   }
 
@@ -193,29 +192,30 @@ class TableView extends React.Component {
     });
   }
 
-    handleStageSelect(stage) {
-        const unSeleceted = this.state.unSelectedStages;
-        const index = unSeleceted.indexOf(stage);
-        unSeleceted.splice(index, 1);
-        const selected = this.state.selectedStages;
-        selected.push(stage);
-        this.setState({
-            unSelectedStages: unSeleceted,
-            selectedStages: selected
-        });
-    }
-    handleStageDeselect(stage) {
-        const selected = this.state.selectedStages;
-        const index = selected.indexOf(stage);
-        selected.splice(index, 1);
-        let unSelected = this.state.unSelectedStages;
-        unSelected.push(stage);
+  handleStageSelect(stage) {
+    const unSeleceted = this.state.unSelectedStages;
+    const index = unSeleceted.indexOf(stage);
+    unSeleceted.splice(index, 1);
+    const selected = this.state.selectedStages;
+    selected.push(stage);
+    this.setState({
+      unSelectedStages: unSeleceted,
+      selectedStages: selected
+    });
+  }
 
-        this.setState({
-            unSelectedStages: unSelected,
-            selectedStages: selected
-        });
-    }
+  handleStageDeselect(stage) {
+    const selected = this.state.selectedStages;
+    const index = selected.indexOf(stage);
+    selected.splice(index, 1);
+    let unSelected = this.state.unSelectedStages;
+    unSelected.push(stage);
+
+    this.setState({
+      unSelectedStages: unSelected,
+      selectedStages: selected
+    });
+  }
 
   updateSearch(event) {
     this.setState({
@@ -223,89 +223,89 @@ class TableView extends React.Component {
     });
   }
 
-    render() {
-        const filteredCompanies= filterCompanies(this.state.companies, this.state.selectedStages,
-            this.state.selectedTags);
-        let searchResults = filteredCompanies.filter((comp) => {
-            return comp.name.toLowerCase().includes(this.state.search.toLowerCase());
-        });
-        return (
-            <div className="container">
-                <div className="row">
-                    <h3>Tags</h3>
-                    {
-                        this.state.unSelectedTags.map((tag, i) => {
-                            return (
-                                <div className="chip" key={i} onClick={(e) => this.handleTagSelect(tag)}>{tag}</div>);
-                        })
-                    }
-                </div>
-                <div className="row">
-                    <h4>Selected tags</h4>
-                    {
-                        this.state.selectedTags.map((tag, i) => {
-                            return (
-                                <div className="chip" key={i} onClick={(e) => this.handleTagDeselect(tag)}>{tag}</div>);
-                        })
-                    }
-                </div>
-                <div className="row">
-                    <h3>Stages</h3>
-                    {
-                        this.state.unSelectedStages.map((stage, i) => {
-                            return (
-                                <div className="chip" key={i} onClick={(e) =>
-                                    this.handleStageSelect(stage)}>{stage}</div>);
-                        })
-                    }
-                </div>
-                <div className="row">
-                    <h4>Selected stages</h4>
-                    {
-                        this.state.selectedStages.map((stage, i) => {
-                            return (
-                                <div className="chip" key={i} onClick={(e) =>
-                                    this.handleStageDeselect(stage)}>{stage}</div>);
-                        })
-                    }
-                </div>
-                <div className="row" >
-                    <input className="text" type="text" placeholder="Search" value={this.state.search}
-                           onChange={(event) => this.updateSearch(event)} />
-                </div >
-                <div className="row">
-                    <div className="table-responsive">
-                        <h3>Table view</h3>
-                        <table className="table">
-                            <thead>
-                            <tr>
-                                <th id='companyCol' onClick={(e) => this.handleNameClick(e)}>Company
-                                    <i className="fa fa-fw fa-sort"/></th>
-                                <th id='descriptionCol'>Description</th>
-                                <th id='fundingCol' onClick={(e) => this.handleFundingClick(e)}>Funding
-                                    <i className="fa fa-fw fa-sort"/></th>
-                                <th id='employeesCol' onClick={(e) => this.handleEmployeesClick(e)}>Employees
-                                    <i className="fa fa-fw fa-sort"/></th>
-                                <th id='tagsCol'>Tags</th>
-                                <th id='stageCol'>Stage</th>
-                                <th id='foundedCol' onClick={(e) => this.handleFoundedClick(e)}>Founded
-                                    <i className="fa fa-fw fa-sort"/></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {searchResults.map((comp, i) => {
-                                if (i <= this.state.showCount)
-                                    return (<CompanyRow key={comp.slug} company={comp}/>);
-                            })}
-                            </tbody>
-                        </table>
-                        <button className='showAll' type="button" onClick={(e) => this.showMore(e)}>Show more</button>
-                        <button className='showAll' type="button" onClick={(e) => this.showAll(e)}>Show all</button>
-                    </div>
-                </div>
+  render() {
+    const filteredCompanies = filterCompanies(this.state.companies, this.state.selectedStages,
+      this.state.selectedTags);
+    let searchResults = filteredCompanies.filter((comp) => {
+      return comp.name.toLowerCase().includes(this.state.search.toLowerCase());
+    });
+    return (
+      <div className="container">
+        <div className="row">
+          <h3>Tags</h3>
+          {
+            this.state.unSelectedTags.map((tag, i) => {
+              return (
+                <div className="chip" key={i} onClick={(e) => this.handleTagSelect(tag)}>{tag}</div>);
+            })
+          }
+        </div>
+        <div className="row">
+          <h4>Selected tags</h4>
+          {
+            this.state.selectedTags.map((tag, i) => {
+              return (
+                <div className="chip" key={i} onClick={(e) => this.handleTagDeselect(tag)}>{tag}</div>);
+            })
+          }
+        </div>
+        <div className="row">
+          <h3>Stages</h3>
+          {
+            this.state.unSelectedStages.map((stage, i) => {
+              return (
+                <div className="chip" key={i} onClick={(e) =>
+                  this.handleStageSelect(stage)}>{stage}</div>);
+            })
+          }
+        </div>
+        <div className="row">
+          <h4>Selected stages</h4>
+          {
+            this.state.selectedStages.map((stage, i) => {
+              return (
+                <div className="chip" key={i} onClick={(e) =>
+                  this.handleStageDeselect(stage)}>{stage}</div>);
+            })
+          }
+        </div>
+        <div className="row">
+          <div className="table-responsive">
+            <h3>Table view</h3>
+            <div className="row" >
+              <input className="text" type="text" placeholder="Search" value={this.state.search}
+                     onChange={(event) => this.updateSearch(event)}/>
             </div>
-        );
-    }
+            <table className="table">
+              <thead>
+              <tr>
+                <th id='companyCol' onClick={(e) => this.handleNameClick(e)}>Company
+                  <i className="fa fa-fw fa-sort"/></th>
+                <th id='descriptionCol'>Description</th>
+                <th id='fundingCol' onClick={(e) => this.handleFundingClick(e)}>Funding
+                  <i className="fa fa-fw fa-sort"/></th>
+                <th id='employeesCol' onClick={(e) => this.handleEmployeesClick(e)}>Employees
+                  <i className="fa fa-fw fa-sort"/></th>
+                <th id='tagsCol'>Tags</th>
+                <th id='stageCol'>Stage</th>
+                <th id='foundedCol' onClick={(e) => this.handleFoundedClick(e)}>Founded
+                  <i className="fa fa-fw fa-sort"/></th>
+              </tr>
+              </thead>
+              <tbody>
+              {searchResults.map((comp, i) => {
+                if (i <= this.state.showCount)
+                  return (<CompanyRow key={comp.slug} company={comp}/>);
+              })}
+              </tbody>
+            </table>
+            <button className='showAll' type="button" onClick={(e) => this.showMore(e)}>Show more</button>
+            <button className='showAll' type="button" onClick={(e) => this.showAll(e)}>Show all</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 TableView.propTypes = {
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,29 @@ var path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: [
-        './src/index.js',
-        './styles/app.css'
-    ],
-    output: {
-        filename: 'dist/bundle.js',
-        path: path.resolve(__dirname, 'dist')
+  entry: [
+    './src/index.js',
+    './styles/app.css'
+  ],
+  output: {
+    filename: 'dist/bundle.js',
+    path: path.resolve(__dirname, 'dist')
 
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract('css-loader'),
-            }
-        ]
-    }
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      { test: /\.js/, use: 'babel-loader' },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract('css-loader'),
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'styles/app.css',
+      allChunks: true
+    })
+  ]
 };
