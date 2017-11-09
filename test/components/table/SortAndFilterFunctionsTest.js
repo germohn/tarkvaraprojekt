a78da2841614cfedd621a47823bf1f738ebfa785
HaraldAstok <haraldastok@gmail.com>
2017-11-09 20:55:12 +0200

    test on changeNumOrder with employees, founded and funding

--- test/components/table/SortAndFilterFunctionsTest.js
+++ test/components/table/SortAndFilterFunctionsTest.js
@@ -1,5 +1,5 @@
 
-import {changeOrder} from '../../../src/containers/util/SortAndFilterFunctions';
+import {changeNumOrder, changeOrder} from '../../../src/containers/util/SortAndFilterFunctions';
 
 describe('Testing changeOrder function, sorting by name ', () => {
   const descendingData = [
@@ -28,5 +28,91 @@ describe('Testing changeOrder function, sorting by name ', () => {
 
   it('When data descending and changed to descending', () => {
     expect(changeOrder(descendingData, 'name', 'desc')).to.eql(descendingData);
+  });
+});
+
+describe('Testing changeNumOrder, employees, founded and funding', () => {
+  const employeesDescendingData = [
+    {slug: 'zeroturnaround', name: 'ZeroTurnaround', employees: 83, foundedOn: '2007-09-07', funding: 16000000},
+    {slug: 'iizi-group', name: 'IIZI group', employees: 75, foundedOn: '2001-01-01', funding: 20285},
+    {slug: 'sportlyzer', name: 'Sportlyzer', employees: 7, foundedOn: '2009-07-09', funding: 1059231},
+    {slug: 'smartly-3', name: 'Smartly.sg', employees: 5, foundedOn: '2014-01-01', funding: 69744},
+    {slug: 'zerply', name: 'Zerply', employees: 3, foundedOn: '2010-04-30', funding: 700000}
+  ];
+  const employeesAscendingData = [
+    {slug: 'zerply', name: 'Zerply', employees: 3, foundedOn: '2010-04-30', funding: 700000},
+    {slug: 'smartly-3', name: 'Smartly.sg', employees: 5, foundedOn: '2014-01-01', funding: 69744},
+    {slug: 'sportlyzer', name: 'Sportlyzer', employees: 7, foundedOn: '2009-07-09', funding: 1059231},
+    {slug: 'iizi-group', name: 'IIZI group', employees: 75, foundedOn: '2001-01-01', funding: 20285},
+    {slug: 'zeroturnaround', name: 'ZeroTurnaround', employees: 83, foundedOn: '2007-09-07', funding: 16000000}
+  ];
+
+  const foundedDescendingData = [
+    {slug: 'smartly-3', name: 'Smartly.sg', employees: 5, foundedOn: '2014-01-01', funding: 69744},
+    {slug: 'zerply', name: 'Zerply', employees: 3, foundedOn: '2010-04-30', funding: 700000},
+    {slug: 'sportlyzer', name: 'Sportlyzer', employees: 7, foundedOn: '2009-07-09', funding: 1059231},
+    {slug: 'zeroturnaround', name: 'ZeroTurnaround', employees: 83, foundedOn: '2007-09-07', funding: 16000000},
+    {slug: 'iizi-group', name: 'IIZI group', employees: 75, foundedOn: '2001-01-01', funding: 20285}
+  ];
+  const foundedAscendingData = [
+    {slug: 'iizi-group', name: 'IIZI group', employees: 75, foundedOn: '2001-01-01', funding: 20285},
+    {slug: 'zeroturnaround', name: 'ZeroTurnaround', employees: 83, foundedOn: '2007-09-07', funding: 16000000},
+    {slug: 'sportlyzer', name: 'Sportlyzer', employees: 7, foundedOn: '2009-07-09', funding: 1059231},
+    {slug: 'zerply', name: 'Zerply', employees: 3, foundedOn: '2010-04-30', funding: 700000},
+    {slug: 'smartly-3', name: 'Smartly.sg', employees: 5, foundedOn: '2014-01-01', funding: 69744}
+  ];
+
+  const fundingDescendingData = [
+    {slug: 'zeroturnaround', name: 'ZeroTurnaround', employees: 83, foundedOn: '2007-09-07', funding: 16000000},
+    {slug: 'sportlyzer', name: 'Sportlyzer', employees: 7, foundedOn: '2009-07-09', funding: 1059231},
+    {slug: 'zerply', name: 'Zerply', employees: 3, foundedOn: '2010-04-30', funding: 700000},
+    {slug: 'smartly-3', name: 'Smartly.sg', employees: 5, foundedOn: '2014-01-01', funding: 69744},
+    {slug: 'iizi-group', name: 'IIZI group', employees: 75, foundedOn: '2001-01-01', funding: 20285}
+  ];
+  const fundingAscendingData = [
+    {slug: 'iizi-group', name: 'IIZI group', employees: 75, foundedOn: '2001-01-01', funding: 20285},
+    {slug: 'smartly-3', name: 'Smartly.sg', employees: 5, foundedOn: '2014-01-01', funding: 69744},
+    {slug: 'zerply', name: 'Zerply', employees: 3, foundedOn: '2010-04-30', funding: 700000},
+    {slug: 'sportlyzer', name: 'Sportlyzer', employees: 7, foundedOn: '2009-07-09', funding: 1059231},
+    {slug: 'zeroturnaround', name: 'ZeroTurnaround', employees: 83, foundedOn: '2007-09-07', funding: 16000000}
+  ];
+
+  it('When employees is descending and changed to ascending', () =>{
+    expect(changeNumOrder(employeesDescendingData, 'employees', 'asc')).to.eql(employeesAscendingData);
+  });
+  it('When employees is ascending and changed to descending', () =>{
+    expect(changeNumOrder(employeesAscendingData, 'employees', 'desc')).to.eql(employeesDescendingData);
+  });
+  it('When employees is descending and changed to descending', () =>{
+    expect(changeNumOrder(employeesDescendingData, 'employees', 'desc')).to.eql(employeesDescendingData);
+  });
+  it('When employees is ascending and changed to ascending', () =>{
+    expect(changeNumOrder(employeesAscendingData, 'employees', 'asc')).to.eql(employeesAscendingData);
+  });
+
+  it('When founded is descending and changed to ascending', () =>{
+    expect(changeNumOrder(foundedDescendingData, 'foundedOn', 'asc')).to.eql(foundedAscendingData);
+  });
+  it('When founded is ascending and changed to descending', () =>{
+    expect(changeNumOrder(foundedAscendingData, 'foundedOn', 'desc')).to.eql(foundedDescendingData);
+  });
+  it('When founded is descending and changed to descending', () =>{
+    expect(changeNumOrder(foundedDescendingData, 'foundedOn', 'desc')).to.eql(foundedDescendingData);
+  });
+  it('When founded is ascending and changed to ascending', () =>{
+    expect(changeNumOrder(foundedAscendingData, 'foundedOn', 'asc')).to.eql(foundedAscendingData);
+  });
+
+  it('When funding is descending and changed to ascending', () =>{
+    expect(changeNumOrder(fundingDescendingData, 'funding', 'asc')).to.eql(fundingAscendingData);
+  });
+  it('When funding is ascending and changed to descending', () =>{
+    expect(changeNumOrder(fundingAscendingData, 'funding', 'desc')).to.eql(fundingDescendingData);
+  });
+  it('When funding is descending and changed to descending', () =>{
+    expect(changeNumOrder(fundingDescendingData, 'funding', 'desc')).to.eql(fundingDescendingData);
+  });
+  it('When funding is ascending and changed to ascending', () =>{
+    expect(changeNumOrder(fundingAscendingData, 'funding', 'asc')).to.eql(fundingAscendingData);
   });
 });
