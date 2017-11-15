2afe319744e6c875b3c232a8842b9a6aeb4a97a8
Madli Urbanik <madliurbanik@gmail.com>
2017-11-16 00:08:06 +0200

    style changes in tableview, see #68, #76, #78

--- src/components/table/CompanyRow.js
+++ src/components/table/CompanyRow.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const getSubstring = (input) => {
+  if (!input) return;
+  const limit = 70;
+  if (input.length < limit) return input;
+  if (input.indexOf(' ', limit * 0.9) == -1) return input;
+  const end = input.indexOf(' ', limit * 0.9);
+  return input.substring(0, end) + '...';
+};
+
 const showYear = (dateString) => {
   try {
     return dateString.substring(0, 4);
@@ -18,6 +27,16 @@ class CompanyRow extends React.Component {
     window.open(link);
   }
 
+  showLogo(link) {
+    if (link == undefined) {
+      return (<img className='logo' src='../styles/img/no-image-icon-23494.png'
+                   alt="logo"/>);
+    } else {
+      return (<img className='logo' src={this.props.company.logo100x100}
+                   alt="logo"/>);
+    }
+  }
+
   render() {
     return (
       <tr className='company-row' onClick={(e) => this.rowClick(this.props.company.url)}>
@@ -25,22 +44,26 @@ class CompanyRow extends React.Component {
           <table>
             <tbody>
             <tr>
-              <td className='borderless'>
-                <img className='logo' src={this.props.company.logo100x100} alt="logo"/>
+              <td className="borderless" rowSpan="2">{this.showLogo(this.props.company.logo100x100)}</td>
+              <td className="borderless">
+                <table>
+                  <tbody>
+                  <tr>
+                    <td><p className='companyName'>{this.props.company.name}</p></td>
+                  </tr>
+                  <tr>
+                    <td id="description" colSpan="2">{getSubstring(this.props.company.description)}</td>
+                  </tr>
+                  </tbody>
+                </table>
               </td>
-              <td className='borderless'>
-                <p className='companyName'>{this.props.company.name}</p>
-              </td>
-            </tr>
-            <tr>
-              <td id="description" colSpan="2">{this.props.company.description}</td>
             </tr>
             </tbody>
           </table>
         </td>
         <td>{this.props.company.funding ? this.props.company.funding.toLocaleString('en-US') + ' $' : '-'}</td>
         <td>{this.props.company.employees ? this.props.company.employees : '-'}</td>
-        <td>{this.props.company.tags ? this.props.company.tags : '-'}</td>
+        <td>{this.props.company.tags ? this.props.company.tags.join(', ') : '-'}</td>
         <td>{this.props.company.stageName}</td>
         <td>{this.props.company.foundedOn ? showYear(this.props.company.foundedOn) : '-'}</td>
       </tr>
