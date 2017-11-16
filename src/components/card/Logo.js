0a76d95d3081da1f20df3b26219643e0771f1933
Madli Urbanik <madliurbanik@gmail.com>
2017-11-17 00:28:28 +0200

    changes in logo componenst structure

--- src/components/card/Logo.js
+++ src/components/card/Logo.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Logo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    if (this.props.url == undefined) {
+const Logo = (props) => {
+  {
+    if (props.url == undefined) {
       return (<img className="logo" src='../styles/img/no-image-icon-23494.jpg'
                    alt="logo"/>);
     } else {
-      return (<img className="logo" src={this.props.url}
+      return (<img className="logo" src={props.url}
                    alt="logo"/>);
     }
   }
-}
+};
 
 Logo.propTypes = {
   url: PropTypes.string.isRequired
 };
+
 export default Logo;
