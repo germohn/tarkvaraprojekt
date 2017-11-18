0d1eba7788188f6f7c1fdcbcc55fb69e558832c1
Helen Hendrikson <helen.hendrikson92@gmail.com>
2017-11-19 01:08:58 +0200

    Adding some tests fro TableView, Buttons, CompanyCard

--- test/containers/table/TableTest.js
+++ test/containers/table/TableTest.js
@@ -88,4 +88,48 @@ describe('Table', () => {
     wrapper.instance().showAll();
     expect(wrapper).to.have.exactly(80).descendants(CompanyRow);
   });
+  it('Test if companycol exists in table and handleNameClick is called when clicked', () => {
+    const noop1 = sinon.spy();
+    const companies = generateFakeCompanies(80);
+    const wrapper = shallow(<TableView data={companies} handleNameClick={noop1}
+                                       handleSortingClick={noop} renderArrow={noop}/>);
+    expect(wrapper.find('#companyCol')).to.exist;
+
+    const modal = wrapper.find('#companyCol');
+    modal.simulate('click');
+    expect(noop1.calledOnce).to.be.true;
+  });
+  it('Test if fundingCol exists in table and handleSortingClick is called when clicked', () => {
+    const noop2 = sinon.spy();
+    const companies = generateFakeCompanies(80);
+    const wrapper = shallow(<TableView data={companies} handleNameClick={noop}
+                                       handleSortingClick={noop2} renderArrow={noop}/>);
+    expect(wrapper.find('#fundingCol')).to.exist;
+
+    const modal = wrapper.find('#fundingCol');
+    modal.simulate('click');
+    expect(noop2.calledOnce).to.be.true;
+  });
+  it('Test if employeesCol exists in table and handleSortingClick is called when clicked', () => {
+    const noop3 = sinon.spy();
+    const companies = generateFakeCompanies(80);
+    const wrapper = shallow(<TableView data={companies} handleNameClick={noop}
+                                       handleSortingClick={noop3} renderArrow={noop}/>);
+    expect(wrapper.find('#employeesCol')).to.exist;
+
+    const modal = wrapper.find('#employeesCol');
+    modal.simulate('click');
+    expect(noop3.calledOnce).to.be.true;
+  });
+  it('Test if foundedCol exists in table and handleSortingClick is called when clicked', () => {
+    const noop4 = sinon.spy();
+    const companies = generateFakeCompanies(80);
+    const wrapper = shallow(<TableView data={companies} handleNameClick={noop}
+                                       handleSortingClick={noop4} renderArrow={noop}/>);
+    expect(wrapper.find('#foundedCol')).to.exist;
+
+    const modal = wrapper.find('#foundedCol');
+    modal.simulate('click');
+    expect(noop4.calledOnce).to.be.true;
+  });
 });
